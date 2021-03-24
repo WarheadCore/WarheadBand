a9453c4f2857de137d6b5c83d9b8d5c1450c9f91
Restyled.io <commits@restyled.io>
2021-03-24 19:40:45 +0000

    Restyled by autopep8

--- apps/EnumUtils/enumutils_describe.py
+++ apps/EnumUtils/enumutils_describe.py
@@ -14,13 +14,16 @@ if not getcwd().endswith('src'):
     print('(Invoke as \'python ../contrib/enumutils_describe.py\')')
     exit(1)
 
-EnumPattern = compile(r'//\s*EnumUtils: DESCRIBE THIS(?:\s*\(in ([^\)]+)\))?\s+enum\s+([0-9A-Za-z]+)[^\n]*\s*{([^}]+)};')
+EnumPattern = compile(
+    r'//\s*EnumUtils: DESCRIBE THIS(?:\s*\(in ([^\)]+)\))?\s+enum\s+([0-9A-Za-z]+)[^\n]*\s*{([^}]+)};')
 EnumValuesPattern = compile(r'\s+\S.+?(,|$)[^\n]*')
 EnumValueNamePattern = compile(r'^\s*([a-zA-Z0-9_]+)', flags=MULTILINE)
 EnumValueSkipLinePattern = compile(r'^\s*//')
 EnumValueCommentPattern = compile(r'//,?[ \t]*([^\n]+)$')
-CommentMatchFormat = compile(r'^(((TITLE +(.+?))|(DESCRIPTION +(.+?))) *){1,2}$')
+CommentMatchFormat = compile(
+    r'^(((TITLE +(.+?))|(DESCRIPTION +(.+?))) *){1,2}$')
 CommentSkipFormat = compile(r'^SKIP *$')
+
 
 def strescape(str):
     res = ''
@@ -31,12 +34,13 @@ def strescape(str):
             res += char
     return '"' + res + '"'
 
+
 def processFile(path, filename):
-    input = open('%s/%s.h' % (path, filename),'r')
+    input = open('%s/%s.h' % (path, filename), 'r')
     if input is None:
         print('Failed to open %s.h' % filename)
         return
-        
+
     file = input.read()
 
     enums = []
@@ -46,22 +50,22 @@ def processFile(path, filename):
         values = []
         for value in EnumValuesPattern.finditer(enum.group(3)):
             valueData = value.group(0)
-            
+
             valueNameMatch = EnumValueNamePattern.search(valueData)
             if valueNameMatch is None:
                 if EnumValueSkipLinePattern.search(valueData) is None:
                     print('Name of value not found: %s' % repr(valueData))
                 continue
             valueName = valueNameMatch.group(1)
-                
+
             valueCommentMatch = EnumValueCommentPattern.search(valueData)
             valueComment = None
             if valueCommentMatch:
                 valueComment = valueCommentMatch.group(1)
-            
+
             valueTitle = None
             valueDescription = None
-            
+
             if valueComment is not None:
                 if CommentSkipFormat.match(valueComment) is not None:
                     continue
@@ -71,20 +75,21 @@ def processFile(path, filename):
                     valueDescription = commentMatch.group(6)
                 else:
                     valueDescription = valueComment
-            
+
             if valueTitle is None:
                 valueTitle = valueName
             if valueDescription is None:
                 valueDescription = ''
-            
+
             values.append((valueName, valueTitle, valueDescription))
-                
+
         enums.append((prefix + name, prefix, values))
-        print('%s.h: Enum %s parsed with %d values' % (filename, name, len(values)))
-        
+        print('%s.h: Enum %s parsed with %d values' %
+              (filename, name, len(values)))
+
     if not enums:
         return
-        
+
     print('Done parsing %s.h (in %s)\n' % (filename, path))
     output = open('%s/enuminfo_%s.cpp' % (path, filename), 'w')
     if output is None:
@@ -101,27 +106,32 @@ def processFile(path, filename):
     output.write('namespace acore::Impl::EnumUtilsImpl\n')
     output.write('{\n')
     for name, prefix, values in enums:
-        tag = ('data for enum \'%s\' in \'%s.h\' auto-generated' % (name, filename))
+        tag = ('data for enum \'%s\' in \'%s.h\' auto-generated' %
+               (name, filename))
         output.write('\n')
         output.write('/*' + ('*'*(len(tag)+2)) + '*\\\n')
         output.write('|* ' + tag + ' *|\n')
         output.write('\\*' + ('*'*(len(tag)+2)) + '*/\n')
         output.write('template <>\n')
-        output.write('EnumText EnumUtils<%s>::ToString(%s value)\n' % (name, name))
+        output.write('EnumText EnumUtils<%s>::ToString(%s value)\n' %
+                     (name, name))
         output.write('{\n')
         output.write('    switch (value)\n')
         output.write('    {\n')
         for label, title, description in values:
-            output.write('        case %s: return { %s, %s, %s };\n' % (prefix + label, strescape(label), strescape(title), strescape(description)))
+            output.write('        case %s: return { %s, %s, %s };\n' % (
+                prefix + label, strescape(label), strescape(title), strescape(description)))
         output.write('        default: throw std::out_of_range("value");\n')
         output.write('    }\n')
         output.write('}\n')
         output.write('\n')
         output.write('template <>\n')
-        output.write('size_t EnumUtils<%s>::Count() { return %d; }\n' % (name, len(values)))
+        output.write(
+            'size_t EnumUtils<%s>::Count() { return %d; }\n' % (name, len(values)))
         output.write('\n')
         output.write('template <>\n')
-        output.write('%s EnumUtils<%s>::FromIndex(size_t index)\n' % (name, name))
+        output.write('%s EnumUtils<%s>::FromIndex(size_t index)\n' %
+                     (name, name))
         output.write('{\n')
         output.write('    switch (index)\n')
         output.write('    {\n')
@@ -132,7 +142,8 @@ def processFile(path, filename):
         output.write('}\n')
         output.write('\n')
         output.write('template <>\n')
-        output.write('size_t EnumUtils<%s>::ToIndex(%s value)\n' % (name, name))
+        output.write('size_t EnumUtils<%s>::ToIndex(%s value)\n' %
+                     (name, name))
         output.write('{\n')
         output.write('    switch (value)\n')
         output.write('    {\n')
@@ -143,6 +154,7 @@ def processFile(path, filename):
         output.write('}\n')
 
     output.write('}\n')
+
 
 FilenamePattern = compile(r'^(.+)\.h$')
 for root, dirs, files in walk('.'):
