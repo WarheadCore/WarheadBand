7d607248543bd07004e6ce31808378f6b6585c0c
Restyled.io <commits@restyled.io>
2021-03-24 19:41:01 +0000

    Restyled by isort

--- apps/EnumUtils/enumutils_describe.py
+++ apps/EnumUtils/enumutils_describe.py
@@ -1,5 +1,5 @@
-from re import compile, MULTILINE
-from os import walk, getcwd
+from os import getcwd, walk
+from re import MULTILINE, compile
 
 notice = """/*
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
