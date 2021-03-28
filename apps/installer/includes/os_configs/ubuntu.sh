35d3fe080f459d1d3f67705442c7f5ae5b2b3778
Restyled.io <commits@restyled.io>
2021-03-28 10:16:34 +0000

    Restyled by shfmt

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -1,20 +1,18 @@
-
-if ! command -v lsb_release &>/dev/null ; then
-       sudo apt-get install -y lsb-release
+if ! command -v lsb_release &>/dev/null; then
+  sudo apt-get install -y lsb-release
 fi
 
-UBUNTU_VERSION=$(lsb_release -sr);
+UBUNTU_VERSION=$(lsb_release -sr)
 
 sudo apt-get update -y
 
 if [[ $CONTINUOUS_INTEGRATION ]]; then
   sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
-  libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-client \
-  libboost-system1.6*-dev libboost-filesystem1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev \
-  libncurses5-dev ccache
+    libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-client \
+    libboost-system1.6*-dev libboost-filesystem1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev \
+    libncurses5-dev ccache
 else
   sudo apt-get install -y git cmake make gcc g++ clang libmysqlclient-dev \
-  libssl-dev libbz2-dev libreadline-dev libncurses-dev \
-  mysql-server libace-6.* libace-dev curl unzip
+    libssl-dev libbz2-dev libreadline-dev libncurses-dev \
+    mysql-server libace-6.* libace-dev curl unzip
 fi
-  
