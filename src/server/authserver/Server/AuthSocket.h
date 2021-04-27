129cec0d5cb8acc55302e6c0618e85d9ef7ebf8b
Winfidonarleyan <dowlandtop@yandex.com>
2021-04-27 21:09:24 +0700

    chore(Core/Auth): add AccountInfo helper
    - Part https://github.com/WarheadCore/Warhead/commit/5bf90b33388bfabb3f7a434980021083d9911a36

--- src/server/authserver/Server/AuthSocket.h
+++ src/server/authserver/Server/AuthSocket.h
@@ -25,6 +25,7 @@
 #include "SRP6.h"
 
 class ACE_INET_Addr;
+class Fields;
 struct Realm;
 
 enum eStatus
@@ -35,6 +36,21 @@ enum eStatus
     STATUS_PATCH,      // unused
     STATUS_AUTHED,
     STATUS_CLOSED
+};
+
+struct AccountInfo
+{
+    void LoadResult(Field* fields);
+
+    uint32 Id = 0;
+    std::string Login;
+    bool IsLockedToIP = false;
+    std::string LockCountry;
+    std::string LastIP;
+    uint32 FailedLogins = 0;
+    bool IsBanned = false;
+    bool IsPermanenetlyBanned = false;
+    AccountTypes SecurityLevel = SEC_PLAYER;
 };
 
 // Handle login commands
@@ -75,8 +91,7 @@ private:
     std::array<uint8, 16> _reconnectProof = {};
 
     eStatus _status;
-
-    std::string _login;
+    AccountInfo _accountInfo;
     Optional<std::vector<uint8>> _totpSecret;
 
     // Since GetLocaleByName() is _NOT_ bijective, we have to store the locale as a string. Otherwise we can't differ
@@ -85,7 +100,6 @@ private:
     std::string _os;
     uint16 _build;
     uint8 _expversion;
-    AccountTypes _accountSecurityLevel;
 };
 
 #endif
