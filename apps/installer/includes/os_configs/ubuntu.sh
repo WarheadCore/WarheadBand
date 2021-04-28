f7d5da975c66aba063dfbbe9b05aa3e0475db7b7
Winfidonarleyan <dowlandtop@yandex.com>
2021-04-28 19:51:10 +0700

    3

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -1,6 +1,6 @@
 
 if ! command -v lsb_release &>/dev/null ; then
-       sudo apt-get install -y lsb-release
+  sudo apt-get install -y lsb-release
 fi
 
 UBUNTU_VERSION=$(lsb_release -sr);
@@ -9,12 +9,13 @@ sudo apt-get update -y
 
 if [[ $CONTINUOUS_INTEGRATION ]]; then
   sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
-  libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-client \
-  libboost-system1.6*-dev libboost-filesystem1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev \
+  libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev mysql-client \
+  libboost-system1.7*-dev libboost-filesystem1.7*-dev libboost-program-options1.7*-dev libboost-iostreams1.7*-dev \
   libncurses5-dev ccache
 else
   sudo apt-get install -y git gcc g++ \
   libssl-dev libbz2-dev libreadline-dev libncurses-dev \
-  mysql-server libace-6.*
+  libboost-system1.7*-dev libboost-filesystem1.7*-dev libboost-program-options1.7*-dev libboost-iostreams1.7*-dev \
+  mysql-server
 fi
 
