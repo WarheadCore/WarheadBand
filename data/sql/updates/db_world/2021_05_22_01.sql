69808277b181b74661b7cc06f750c133b0ddeb7a
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-26 02:34:12 +0700

    Revert "fix(DB/Broadcasttext): correct column name"
    This reverts commit a34bde08fa7ca1afd06ad3cd10b21822f5b77749.

--- data/sql/updates/db_world/2021_05_22_01.sql
+++ data/sql/updates/db_world/2021_05_22_01.sql
@@ -22,7 +22,7 @@ UPDATE `creature_text` SET `emote`=5 WHERE `creatureid`=17312 AND `groupid`=4;
 
 DELETE FROM `creature_text` WHERE `creatureid`=17311;
 INSERT INTO `creature_text` (`creatureid`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
-(17311, 0, 0, (SELECT `Text1` FROM `broadcast_text` WHERE `ID`=13749), 12, 7, 100, 0, 0, 0, 13749, 0, "Cowlen");
+(17311, 0, 0, (SELECT `MaleText` FROM `broadcast_text` WHERE `ID`=13749), 12, 7, 100, 0, 0, 0, 13749, 0, "Cowlen");
 
 UPDATE `script_waypoint` SET `waittime`=3000 WHERE `entry`=17312 AND `pointid`=28;
 UPDATE `script_waypoint` SET `waittime`=5000 WHERE `entry`=17312 AND `pointid`=29;
