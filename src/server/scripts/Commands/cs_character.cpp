45cbd36199421d4a415a411c6084de65728c6db9
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-29 22:29:57 +0700

    fix(Core/Build): followup 370ef82

--- src/server/scripts/Commands/cs_character.cpp
+++ src/server/scripts/Commands/cs_character.cpp
@@ -31,6 +31,10 @@ EndScriptData */
 #include "ReputationMgr.h"
 #include "ScriptMgr.h"
 
+#if WARHEAD_COMPILER == WARHEAD_COMPILER_GNU
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 using namespace Warhead::ChatCommands;
 
 class character_commandscript : public CommandScript
