9fb11c12398da4befe213811da16d9e238efd470
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-31 04:43:09 +0700

    chore(Core/Containers): delete dublicate MapGetValuePtr

--- src/common/Utilities/Containers.h
+++ src/common/Utilities/Containers.h
@@ -169,16 +169,6 @@ namespace Warhead::Containers
     }
 
     /**
-     * Returns a pointer to mapped value (or the value itself if map stores pointers)
-     */
-    template<class M>
-    inline auto MapGetValuePtr(M& map, typename M::key_type const& key) -> decltype(AddressOrSelf(map.find(key)->second))
-    {
-        auto itr = map.find(key);
-        return itr != map.end() ? AddressOrSelf(itr->second) : nullptr;
-    }
-
-    /**
      * @fn void Warhead::Containers::RandomShuffle(C& container)
      *
      * @brief Reorder the elements of the container randomly.
