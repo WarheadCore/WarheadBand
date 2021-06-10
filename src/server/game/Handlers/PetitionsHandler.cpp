77f552a5d993b8ee3d16dadc3ca24bb42d2df722
Winfidonarleyan <dowlandtop@yandex.com>
2021-06-10 09:41:38 +0700

    5

--- src/server/game/Handlers/PetitionsHandler.cpp
+++ src/server/game/Handlers/PetitionsHandler.cpp
@@ -610,7 +610,7 @@ void WorldSession::HandleOfferPetitionOpcode(WorldPacket& recvData)
 
     if (petition->petitionType != GUILD_CHARTER_TYPE)
     {
-        if (!_player->IsMaxLevel())
+        if (!player->IsMaxLevel())
         {
             // player is too low level to join an arena team
             SendArenaTeamCommandResult(ERR_ARENA_TEAM_CREATE_S, player->GetName().c_str(), "", ERR_ARENA_TEAM_TARGET_TOO_LOW_S);
