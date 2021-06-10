dece0bc21fdd2863fe26814bc086ee49eb77f70f
Winfidonarleyan <dowlandtop@yandex.com>
2021-06-11 06:11:58 +0700

    fix(SQL): correct id for LANG_PINFO_MUTED

--- data/sql/custom/db_world/2021_06_10_01_mute.sql
+++ data/sql/custom/db_world/2021_06_10_01_mute.sql
@@ -1,2 +1,2 @@
 -- Replace LANG_PINFO_MUTED
-UPDATE `acore_string` SET `content_default` = '│ Muted: (Reason: %s, Time: %s, Left %s, By: %s)' WHERE `entry` = 550;
+UPDATE `acore_string` SET `content_default` = '│ Muted: (Reason: %s, Time: %s, Left %s, By: %s)' WHERE `entry` = 35403;
