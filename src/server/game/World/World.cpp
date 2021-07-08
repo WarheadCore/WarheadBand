2a8e167a088af5c9cbe519606d61f3e2eefd4854
Winfidonarleyan <dowlandtop@yandex.com>
2021-07-08 23:14:04 +0700

    fix(Core/World): correct GetSecurity in DoForAllGM

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2650,7 +2650,7 @@ void World::DoForAllGM(Worker&& worker)
         if (!player || !player->IsInWorld())
             return;
 
-        if (AccountMgr::IsPlayerAccount(player->GetSecurity()))
+        if (AccountMgr::IsPlayerAccount(player->GetSession()->GetSecurity()))
             continue;
 
         worker(player);
