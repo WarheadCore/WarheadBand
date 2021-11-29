888a25a08a9da9f24131ecae2a9d896ca872e425
Kargatum <dowlandtop@yandex.com>
2021-11-29 21:20:15 +0700

    feat(Core/Config): implement loading files optional  (#57)

--- src/common/Configuration/Config.h
+++ src/common/Configuration/Config.h
@@ -20,7 +20,7 @@
 
 #include "Define.h"
 #include <stdexcept>
-#include <string>
+#include <string_view>
 #include <vector>
 
 class WH_COMMON_API ConfigMgr
@@ -31,15 +31,15 @@ class WH_COMMON_API ConfigMgr
     ~ConfigMgr() = default;
 
 public:
-    bool LoadAppConfigs();
-    bool LoadModulesConfigs();
+    bool LoadAppConfigs(bool isReload = false);
+    bool LoadModulesConfigs(bool isReload = false, bool isNeedPrintInfo = true);
     void Configure(std::string const& initFileName, std::vector<std::string> args, std::string const& modulesConfigList = "");
 
     static ConfigMgr* instance();
 
     bool Reload();
 
-    std::string const& GetFilename();
+    std::string const GetFilename();
     std::string const GetConfigPath();
     std::vector<std::string> const& GetArguments() const;
     std::vector<std::string> GetKeysByString(std::string const& name);
@@ -47,35 +47,13 @@ public:
     template<class T>
     T GetOption(std::string const& name, T const& def, bool showLogs = true) const;
 
-    /*
-     * Deprecated geters. This geters will be deleted
-     */
-
-    // @deprecated DO NOT USE - use GetOption<std::string> instead.
-    std::string GetStringDefault(std::string const& name, const std::string& def, bool showLogs = true);
-
-    // @deprecated DO NOT USE - use GetOption<bool> instead.
-    bool GetBoolDefault(std::string const& name, bool def, bool showLogs = true);
-
-    // @deprecated DO NOT USE - use GetOption<int32> instead.
-    int GetIntDefault(std::string const& name, int def, bool showLogs = true);
-
-    // @deprecated DO NOT USE - use GetOption<float> instead.
-    float GetFloatDefault(std::string const& name, float def, bool showLogs = true);
-
-    /*
-     * End deprecated geters
-     */
-
     bool isDryRun() { return dryRun; }
     void setDryRun(bool mode) { dryRun = mode; }
 
-    void PrintLoadedModulesConfigs();
-
 private:
     /// Method used only for loading main configuration files (authserver.conf and worldserver.conf)
-    bool LoadInitial(std::string const& file);
-    bool LoadAdditionalFile(std::string file);
+    bool LoadInitial(std::string const& file, bool isReload = false);
+    bool LoadAdditionalFile(std::string file, bool isOptional = false, bool isReload = false);
 
     template<class T>
     T GetValueDefault(std::string const& name, T const& def, bool showLogs = true) const;
--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -191,13 +191,10 @@ int main(int argc, char** argv)
     }
 
     // Add file and args in config
-    sConfigMgr->Configure(configFile, std::vector<std::string>(argv, argv + argc), WH_MODULE_CONFIG_LIST);
+    sConfigMgr->Configure(configFile, { argv, argv + argc }, WH_MODULE_CONFIG_LIST);
 
     if (!sConfigMgr->LoadAppConfigs())
         return 1;
-
-    // Loading modules configs
-    sConfigMgr->LoadModulesConfigs();
 
     std::shared_ptr<Warhead::Asio::IoContext> ioContext = std::make_shared<Warhead::Asio::IoContext>();
 
@@ -308,7 +305,7 @@ int main(int argc, char** argv)
         sMetric->Unload();
     });
 
-    // Loading modules configs
+    // Loading modules configs before scripts
     sConfigMgr->LoadModulesConfigs();
 
     ///- Initialize the World
@@ -449,7 +446,7 @@ bool StartDB()
         return false;
 
     ///- Get the realm Id from the configuration file
-    realm.Id.Realm = sConfigMgr->GetIntDefault("RealmID", 0);
+    realm.Id.Realm = sConfigMgr->GetOption<uint32>("RealmID", 0);
     if (!realm.Id.Realm)
     {
         LOG_ERROR("server.worldserver", "Realm ID not defined in configuration file");
