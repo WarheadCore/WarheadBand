3d301252a59e7f93ad1992fefacf2b74c5304696
Kargatum <dowlandtop@yandex.com>
2022-04-15 03:58:12 +0700

    Update src/server/authserver/Server/AuthSession.cpp

--- src/server/authserver/Server/AuthSession.cpp
+++ src/server/authserver/Server/AuthSession.cpp
@@ -837,7 +837,7 @@ bool AuthSession::VerifyVersion(uint8 const* a, int32 aLength, Warhead::Crypto::
         return true;
 
     Warhead::Crypto::SHA1::Digest zeros{};
-    Warhead::Crypto::SHA1::Digest const* versionHash = nullptr;
+    Warhead::Crypto::SHA1::Digest const* versionHash{ nullptr };
 
     if (!isReconnect)
     {
