1420348f9d2bc494acc53d1b970596ca4d1c8d4c
Winfidonarleyan <dowlandtop@yandex.com>
2022-05-03 05:52:45 +0700

    2

--- apps/ci/ci-configure-world.sh
+++ apps/ci/ci-configure-world.sh
@@ -5,24 +5,18 @@ set -e
 cat >>conf/config.sh <<CONFIG_SH
 MTHREADS=$(($(grep -c ^processor /proc/cpuinfo) + 2))
 CWARNINGS=ON
-CDEBUG=OFF
 CTYPE=RelWithDebInfo
-CSCRIPTS=$1
-CMODULES=$2
+CSCRIPTS=static
+CMODULES=$1
 CBUILD_TESTING=OFF
-CAPPS_BUILD=$3
-CTOOLS=ON
-CSCRIPTPCH=$4
-CCOREPCH=$5
+CCOREPCH=$2
+CSCRIPTPCH=$3
+CBUILD_APPS_LIST='-DAPP_WORLDSERVER="enabled"'
 CCUSTOMOPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror"'
 DB_CHARACTERS_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_AUTH_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_WORLD_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 CONFIG_SH
-
-if [[ $ENABLE_CPP_20 -eq 1 ]]; then
-  echo "CUSE_CPP_20=ON" >> ./conf/config.sh
-fi
 
 # Start mysql
 sudo systemctl start mysql
@@ -34,12 +28,6 @@ case $COMPILER in
     time sudo apt-get install -y clang
     echo "CCOMPILERC=\"clang\"" >> ./conf/config.sh
     echo "CCOMPILERCXX=\"clang++\"" >> ./conf/config.sh
-    ;;
-
-  "clang11" )
-    time sudo apt-get install -y clang-11
-    echo "CCOMPILERC=\"clang-11\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-11\"" >> ./conf/config.sh
     ;;
 
   "clang12" )
