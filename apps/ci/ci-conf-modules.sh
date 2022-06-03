3faff3fe5f4a64ff578ffd21885108c00ec135cc
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-03 22:40:11 +0700

    1

--- apps/ci/ci-conf-modules.sh
+++ apps/ci/ci-conf-modules.sh
@@ -4,21 +4,17 @@ set -e
 
 cat >>conf/config.sh <<CONFIG_SH
 MTHREADS=$(($(grep -c ^processor /proc/cpuinfo) + 2))
-CTYPE=RelWithDebInfo
 CCOREPCH=OFF
 CSCRIPTPCH=OFF
-CBUILD_APPS_LIST='-DAPP_WORLDSERVER=disabled'
+CMODULES=static
+CAPPS_BUILD=world-only
 CCUSTOMOPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror"'
 DB_CHARACTERS_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_AUTH_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_WORLD_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 CONFIG_SH
 
-# Start mysql
-sudo systemctl start mysql
-
 case $COMPILER in
-
   # this is in order to use the "default" clang version of the OS, without forcing a specific version
   "clang" )
     time sudo apt-get install -y clang
