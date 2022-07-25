a381713b3d6d28abf41b5f89b7519c14fac2acdd
Winfidonarleyan <dowlandtop@yandex.com>
2022-07-25 22:08:54 +0700

    2

--- src/common/Collision/BoundingIntervalHierarchy.cpp
+++ src/common/Collision/BoundingIntervalHierarchy.cpp
@@ -52,8 +52,8 @@ void BIH::subdivide(int left, int right, std::vector<uint32>& tempTree, buildDat
 
     // calculate extents
     int axis = -1, prevAxis, rightOrig;
-    float clipL{ G3D::fnan() }, clipR{ clipL }, prevClip{ clipL }, split{ clipL };
-    float prevSplit;
+    float clipL{ G3D::fnan() };
+    float clipR, prevClip, split, prevSplit = clipL;
     bool wasLeft = true;
 
     while (true)
