4c1f972bcd934c92ba6e3524c1c2b29b1af4acc4
Winfidonarleyan <dowlandtop@yandex.com>
2022-08-01 04:06:38 +0700

    fix(Core/Build): correct non-pch build

--- src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_moam.cpp
+++ src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_moam.cpp
@@ -18,6 +18,7 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
+#include "Player.h"
 #include "ScriptObject.h"
 #include "ScriptedCreature.h"
 #include "SpellScript.h"
