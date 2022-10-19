c664700aa057c813d5f8440e1aff1b947fe95a11
Winfidonarleyan <dowlandtop@yandex.com>
2022-10-19 11:54:46 +0700

    fix(DB): correct update `2022_10_14_00.sql` for world db

--- data/sql/updates/db_world/2022_10_14_00.sql
+++ data/sql/updates/db_world/2022_10_14_00.sql
@@ -1,5 +1,5 @@
 -- DB update 2022_10_13_00 -> 2022_10_14_00
-UPDATE `broadcast_text_locale` SET `MaleText`="Esta es nuestra batalla final. El paso del tiempo se hará eco de lo que aquí ocurra. Sin importar el resultado, sabrán que luchamos con honor. ¡Que luchamos por la libertad y la seguridad de nuestro pueblo!" WHERE `ID`=36923 AND `locale` IN ('esEs', 'esMX');
+-- UPDATE `broadcast_text_locale` SET `MaleText`="Esta es nuestra batalla final. El paso del tiempo se hará eco de lo que aquí ocurra. Sin importar el resultado, sabrán que luchamos con honor. ¡Que luchamos por la libertad y la seguridad de nuestro pueblo!" WHERE `ID`=36923 AND `locale` IN ('esEs', 'esMX');
 
 UPDATE `creature_text` SET `BroadcastTextId`=37037 WHERE `CreatureID`=38607 AND `GroupID`=13 AND `ID`=0;
 UPDATE `creature_text` SET `BroadcastTextId`=37043 WHERE `CreatureID`=38607 AND `GroupID`=14 AND `ID`=0;
@@ -8,6 +8,5 @@ UPDATE `creature_text` SET `BroadcastTextId`=37046 WHERE `CreatureID`=38607 AND 
 UPDATE `creature_template` SET `gossip_menu_id`=10953, `npcflag`=`npcflag`|1 WHERE `entry`=37187;
 
 DELETE FROM `gossip_menu` WHERE `MenuID`=10953;
-
 INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
 (10953, 15217);
