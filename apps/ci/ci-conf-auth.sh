c320263e559d10952e849348398f8d4fc2e9c7ac
Winfidonarleyan <dowlandtop@yandex.com>
2022-12-23 13:49:25 +0700

    1

--- apps/ci/ci-conf-auth.sh
+++ apps/ci/ci-conf-auth.sh
@@ -8,9 +8,6 @@ CCOREPCH=OFF
 CSCRIPTPCH=OFF
 CAPPS_BUILD=auth-only
 CCUSTOMOPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror"'
-DB_CHARACTERS_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
-DB_AUTH_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
-DB_WORLD_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 CONFIG_SH
 
 case $COMPILER in
@@ -20,18 +17,6 @@ case $COMPILER in
     echo "CCOMPILERC=\"clang\"" >> ./conf/config.sh
     echo "CCOMPILERCXX=\"clang++\"" >> ./conf/config.sh
     ;;
-
-  "clang12" )
-    time sudo apt-get install -y clang-12
-    echo "CCOMPILERC=\"clang-12\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-12\"" >> ./conf/config.sh
-    ;;
-
-  "clang14" )
-      time sudo apt-get install -y clang-14
-      echo "CCOMPILERC=\"clang-14\"" >> ./conf/config.sh
-      echo "CCOMPILERCXX=\"clang++-14\"" >> ./conf/config.sh
-      ;;
 
   * )
     echo "Unknown compiler $COMPILER"
