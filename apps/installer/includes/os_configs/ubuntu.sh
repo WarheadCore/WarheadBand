6e89cc885946455d07c5df747ea37bd05acab435
Winfidonarleyan <dowlandtop@yandex.com>
2022-12-23 13:55:50 +0700

    feat(CI): switch to `Ubunty 22.04` (#157)

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -4,15 +4,17 @@ fi
 
 # UBUNTU_VERSION=$(lsb_release -sr);
 
+# Fix libs
+sudo apt-get install -f
+
 # Added repo for newest lib
-sudo add-apt-repository -y ppa:mhier/libboost-latest
 sudo apt update
 
-# Insstall boost 1.74 from ppa:mhier/libboost-latest for all os versions
-sudo apt-get -y install libboost1.74-dev
+# Install boost
+sudo apt-get -y install libboost-all-dev
 
 if [[ $CONTINUOUS_INTEGRATION ]]; then
-  sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
+  sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl \
   libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev \
   libncurses5-dev ccache
 else
