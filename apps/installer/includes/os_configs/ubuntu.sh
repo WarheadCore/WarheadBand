041c18c6f5b9aac47eda1ba6a787d76111ac5bf9
Winfidonarleyan <dowlandtop@yandex.com>
2022-12-23 13:31:05 +0700

    1

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -4,6 +4,9 @@ fi
 
 # UBUNTU_VERSION=$(lsb_release -sr);
 
+sudo apt-get remove -y libunwind-14
+sudo apt-get install -y libunwind-dev
+
 # Added repo for newest lib
 sudo apt update
 
@@ -11,7 +14,7 @@ sudo apt update
 sudo apt-get -y install libboost-all-dev
 
 if [[ $CONTINUOUS_INTEGRATION ]]; then
-  sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
+  sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl \
   libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev \
   libncurses5-dev ccache
 else
