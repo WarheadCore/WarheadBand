5420b5d46a4f03c49b9a25c4ca29426c443af0d7
Winfidonarleyan <dowlandtop@yandex.com>
2023-10-01 16:26:44 +0700

    fix(Modules/QuestBuff): correct db files

--- modules/mod-quest-buff/sql/db_world/wh_quest_buff.sql
+++ modules/mod-quest-buff/sql/db_world/wh_quest_buff.sql
@@ -1,26 +1,7 @@
--- MariaDB dump 10.19-11.0.2-MariaDB, for Linux (x86_64)
---
--- ------------------------------------------------------
--- Server version	10.5.19-MariaDB-0+deb11u2-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `quest_buff`
 --
-
 DROP TABLE IF EXISTS `wh_quest_buff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wh_quest_buff` (
   `QuestID` int(11) NOT NULL,
   `SpellID` int(11) NOT NULL,
@@ -28,14 +9,10 @@ CREATE TABLE `wh_quest_buff` (
   `Category` int(11) NOT NULL,
   PRIMARY KEY (`QuestID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `quest_buff`
 --
-
-LOCK TABLES `wh_quest_buff` WRITE;
-/*!40000 ALTER TABLE `quest_buff` DISABLE KEYS */;
 INSERT INTO `wh_quest_buff` VALUES
 (51001,700311,1,1),
 (51002,700312,2,1),
@@ -61,16 +38,3 @@ INSERT INTO `wh_quest_buff` VALUES
 (51026,700336,6,3),
 (51027,700337,7,3),
 (51028,700338,8,3);
-/*!40000 ALTER TABLE `quest_buff` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-08-26 20:17:04
